a6b21b1d5bc099076e7d0c10437c46e811417eda
Deniz İrgin <deniz_irgin@yahoo.com>
2018-05-03 00:09:54 +0300

    - database update
     - Meetups, sponsors added

--- src/CodefictionApi/Server/Data/Database.cs
+++ src/CodefictionApi/Server/Data/Database.cs
@@ -12,5 +12,13 @@ namespace Codefiction.CodefictionTech.CodefictionApi.Server.Data
         public P2P[] P2Ps { get; set; }
 
         public Special[] Specials { get; set; }
+
+        public Meetup[] Meetups { get; set; }
+
+        public Video[] Videos { get; set; }
+
+        public Person[] People { get; set; }
+
+        public Sponsor[] Sponsors { get; set; }
     }
 }
---
+++ src/CodefictionApi/Server/Data/Sponsor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Codefiction.CodefictionTech.CodefictionApi.Server.Data
+{
+    public class Sponsor
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string LogoUrl { get; set; }
+    }
+}
